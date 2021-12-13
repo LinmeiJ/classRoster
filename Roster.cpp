dc1afca0ce8deaea0d46a0427e5e0f8c571427cf
LinmeiJ <linmeijiang@hotmail.com>
2021-12-13 12:14:07 -0500

    delete personal info

--- Roster.cpp
+++ Roster.cpp
@@ -25,9 +25,7 @@ void Roster::init(){
 		string studentData[] = {"A1,John,Smith,John1989@gmail.com,20,30,35,40,SECURITY",
 								"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 						     	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-								"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-								"A5,Linmei,Mills,linmeijiang@hotmail.com,25,15,28,66,SOFTWARE"}; // student personal information added to the data table here
-
+								"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY"};
 		//get the size of studentData array
 		int size = *(&studentData + 1) - studentData;
 		int i = 0;
